a647eb8835665acb1ecbe8e154b5616b939f3b93
Victor Humberto <tormentavictor@gmail.com>
2025-08-26 00:03:35 -0300

    feat: add Link component

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -4,6 +4,7 @@ import { MaterialIcons } from "@expo/vector-icons";
 import { styles } from "@/app/index/styles";
 import { colors } from "@/styles/colors";
 import { Categories } from "@/components/categories";
+import { Link } from "@/components/link";
 
 export default function Index() {
   return (
@@ -17,6 +18,12 @@ export default function Index() {
       </View>
 
       <Categories />
+
+      <Link
+        name="Rocketseat"
+        url="https://www.rocketseat.com.br"
+        onDetails={() => console.log("Clicou!!")}
+      />
     </View>
   );
 }
