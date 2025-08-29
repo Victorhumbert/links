70a73d3af3372b60b8f3a2a323072ca6346c8e33
Victor Humberto <tormentavictor@gmail.com>
2025-08-29 14:45:26 -0300

    feat: created save and get methods to links

---
+++ src/storage/link-storage.ts
@@ -0,0 +1,28 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const LINKS_STORAGE_KEY = "links-storage";
+
+type LinkStorage = {
+  id: string;
+  name: string;
+  url: string;
+  category: string;
+};
+
+async function get(): Promise<LinkStorage[]> {
+  const storage = await AsyncStorage.getItem(LINKS_STORAGE_KEY);
+  return storage ? JSON.parse(storage) : [];
+}
+
+async function save(newLink: LinkStorage) {
+  try {
+    const storage = await get();
+    const updated = JSON.stringify([...storage, newLink]);
+
+    await AsyncStorage.setItem(LINKS_STORAGE_KEY, updated);
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const linkStorage = { get, save };
